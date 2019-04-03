4cc49effc151d5b0343ef41606cb69ae23d09d5f
Kyle Babcock <kylerbabcock@gmail.com>
2019-04-02 21:06:17 -0400

    all routes work, need to figure out pushing to friends array

--- routing/apiRoutes.js
+++
@@ -1,6 +0,0 @@
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get("/api/friends", function(req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
-  });
--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 // Dependencies
 var express = require("express");
+var router = express.Router();
 var path = require("path");
 
 // Intializes express app
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/htmlRoutes.js")(app);
+require("./app/routing/apiRoutes.js")(app);
+
+
+app.post("/api/friends", function(req, res) {
+  var newfriend = req.body;
+
+  console.log(newfriend);
+  friends.push(newfriend);
+})
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
